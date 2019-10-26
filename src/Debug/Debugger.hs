65e9dd674441f22116a97a570ecab1e925c6b36e
Callum Lowcay <cwslowcay@gmail.com>
2019-10-26 22:45:07 +1300

    Add simple profiling to the debugger

--- src/Debug/Debugger.hs
+++ src/Debug/Debugger.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MonoLocalBinds #-}
 
 module Debug.Debugger
@@ -29,6 +30,8 @@ import           GBC.Decode
 import           GBC.Graphics
 import           GBC.ISA
 import           GBC.Memory
+import           Numeric
+import qualified SDL.Time                      as SDL
 
 {-# SPECIALIZE readPC :: ReaderT DebugState IO Word16 #-}
 {-# SPECIALIZE busStep :: ReaderT DebugState IO BusEvent #-}
@@ -114,24 +117,24 @@ type BreakPostCondition env m = BusEvent -> ReaderT env m Bool
 -- | Break after a certain number of instructions have been executed.
 breakOnCountOf :: MonadIO m => Int -> ReaderT env m (BreakPreCondition env m)
 breakOnCountOf n0 = do
-  counter <- liftIO $ newIORef n0
+  counter <- liftIO (newIORef n0)
   pure . liftIO $ do
     c <- readIORef counter
     if c == 0
       then pure True
       else do
-        writeIORef counter $ c - 1
+        writeIORef counter (c - 1)
         pure False
 
 -- | Break when breakpoints are triggered.
 breakOnBreakpoints :: UsesDebug env m => ReaderT env m (BreakPostCondition env m)
 breakOnBreakpoints = do
   DebugState {..} <- asks forDebugState
-  pure . const $ shouldBreak breakpoints
+  pure (const (shouldBreak breakpoints))
 
 -- | Break when the PC equals a certain value.
 breakOnPC :: UsesCPU env m => Word16 -> BreakPostCondition env m
-breakOnPC pc = const $ (pc ==) <$> readPC
+breakOnPC pc = const ((pc ==) <$> readPC)
 
 -- | Break when a RET instruction is executed with the current stack pointer.
 breakOnRet :: UsesCPU env m => Word16 -> BreakPreCondition env m
@@ -139,27 +142,28 @@ breakOnRet originalSP = do
   sp          <- readR16 RegSP
   instruction <- decodeOnly decode
   case instruction of
-    RET               -> pure $ originalSP == sp
+    RET               -> pure (originalSP == sp)
     (RETCC condition) -> do
       shouldRet <- testCondition condition
-      pure $ shouldRet && originalSP == sp
-    RETI -> pure $ originalSP == sp
+      pure (shouldRet && originalSP == sp)
+    RETI -> pure (originalSP == sp)
     _    -> pure False
 
 -- | Run the interpreter until one of the break conditions is met.
 doRun
-  :: UsesBus env m => [BreakPreCondition env m] -> [BreakPostCondition env m] -> ReaderT env m ()
-doRun preConditions postConditions = clearBreakFlag >> go
+  :: UsesBus env m => [BreakPreCondition env m] -> [BreakPostCondition env m] -> ReaderT env m Int
+doRun preConditions postConditions = clearBreakFlag >> go 0
  where
-  go = do
+  go !ticks = do
     doPreBreak <- orM preConditions
     if doPreBreak
-      then pure ()
+      then pure ticks
       else do
-        debugInfo   <- busStep
+        debugInfo <- busStep
+        let ticks' = ticks + clockAdvance debugInfo
         breakFlag   <- isBreakFlagSet
-        doPostBreak <- orM $ fmap ($ debugInfo) postConditions
-        if doPostBreak || breakFlag then pure () else go
+        doPostBreak <- orM (fmap ($ debugInfo) postConditions)
+        if doPostBreak || breakFlag then pure ticks' else go ticks'
 
 -- | Disassemble the next 4 instructions at the current PC.
 disassembleAtPC :: UsesDebug env m => ReaderT env m ()
@@ -175,17 +179,31 @@ withAddress (LabelAddress label) action = do
   symbols <- getCodeMap
   case lookupBySymbol symbols label of
     Just addr -> action addr
-    Nothing   -> liftIO $ putStrLn $ "Unknown symbol " ++ label
+    Nothing   -> liftIO (putStrLn ("Unknown symbol " ++ label))
+
+reportingClockStats :: MonadIO m => m Int -> m ()
+reportingClockStats action = do
+  t0     <- SDL.ticks
+  cycles <- action
+  t1     <- SDL.ticks
+  let duration       = t1 - t0
+  let expectedCycles = (4194304.0 * fromIntegral duration) / 1000.0 :: Double
+  let percentSpeed   = (fromIntegral cycles / expectedCycles) * 100.0
+
+  liftIO $ do
+    putStrLn (show cycles ++ " cycles in " ++ show duration ++ "ms")
+    putStrLn ("Clock rate = " ++ show ((cycles * 1000) `div` fromIntegral duration) ++ "Hz")
+    putStrLn ("Relative to expected clock " ++ showFFloat (Just 2) percentSpeed "" ++ "% speed")
 
 -- | Execute a debugger command.
 {-# SPECIALIZE doCommand :: Command -> ReaderT DebugState IO () #-}
 doCommand :: UsesDebug env m => Command -> ReaderT env m ()
 doCommand ShowHeader = do
   mem <- asks forMemory
-  liftIO . dumpHeader $ getROMHeader mem
+  liftIO (dumpHeader (getROMHeader mem))
 doCommand ShowRegs = do
   registerFile <- getRegisterFile
-  liftIO $ dumpRegisters registerFile
+  liftIO (dumpRegisters registerFile)
 doCommand (ShowMem         addr   ) = withAddress addr dumpMem
 doCommand (ShowDisassembly Nothing) = do
   pc        <- readPC
@@ -199,23 +217,23 @@ doCommand (ShowDisassembly (Just addr)) = do
 doCommand (Step n) = do
   preConditions  <- sequence [breakOnCountOf n]
   postConditions <- sequence [breakOnBreakpoints]
-  doRun preConditions postConditions
+  reportingClockStats (doRun preConditions postConditions)
   disassembleAtPC
 doCommand StepOut = do
   sp             <- readR16 RegSP
   postConditions <- sequence [breakOnBreakpoints]
-  doRun [breakOnRet sp] postConditions
+  reportingClockStats (doRun [breakOnRet sp] postConditions)
   DebugState {..} <- asks forDebugState
   wasBreakpoint   <- shouldBreak breakpoints
-  unless wasBreakpoint $ void busStep
+  unless wasBreakpoint (void busStep)
   disassembleAtPC
 doCommand Run = do
   postConditions <- sequence [breakOnBreakpoints]
-  doRun [] postConditions
+  reportingClockStats (doRun [] postConditions)
   disassembleAtPC
 doCommand (RunTo breakAddr) = withAddress breakAddr $ \addr -> do
-  postConditions <- sequence [breakOnBreakpoints, pure $ breakOnPC addr]
-  doRun [] postConditions
+  postConditions <- sequence [breakOnBreakpoints, pure (breakOnPC addr)]
+  reportingClockStats (doRun [] postConditions)
   disassembleAtPC
 doCommand Reset                = reset
 doCommand (Poke8   addr value) = withAddress addr $ \actualAddr -> writeByte actualAddr value
@@ -223,30 +241,30 @@ doCommand (PokeR8  reg  value) = writeR8 reg value
 doCommand (PokeR16 reg  value) = writeR16 reg value
 doCommand (AddBreakpoint addr) = withAddress addr $ \breakAddr -> do
   DebugState {..} <- asks forDebugState
-  liftIO $ setBreakpoint breakpoints breakAddr True
+  liftIO (setBreakpoint breakpoints breakAddr True)
 doCommand (DeleteBreakpoint addr) = withAddress addr $ \breakAddr -> do
   DebugState {..} <- asks forDebugState
-  liftIO $ clearBreakpoint breakpoints breakAddr
+  liftIO (clearBreakpoint breakpoints breakAddr)
 doCommand (DisableBreakpiont addr) = withAddress addr $ \breakAddr -> do
   DebugState {..} <- asks forDebugState
-  liftIO $ setBreakpoint breakpoints breakAddr False
+  liftIO (setBreakpoint breakpoints breakAddr False)
 doCommand ListBreakpoints = do
   DebugState {..} <- asks forDebugState
   liftIO $ do
     allBreakpoints <- listBreakpoints breakpoints
     when (null allBreakpoints) $ putStrLn "No breakpoints set"
     for_ allBreakpoints
-      $ \(addr, enabled) -> putStrLn $ formatHex addr ++ if not enabled then " (disabled)" else ""
+      $ \(addr, enabled) -> putStrLn (formatHex addr ++ if not enabled then " (disabled)" else "")
 doCommand (AddSymbol symbol value) = do
   DebugState {..} <- asks forDebugState
   codeMap'        <- addToMap (symbol, value) <$> getCodeMap
-  liftIO $ saveMap (mapFileName romFile) codeMap'
+  liftIO (saveMap (mapFileName romFile) codeMap')
   setCodeMap codeMap'
 doCommand (DeleteSymbol symbol) = do
   DebugState {..} <- asks forDebugState
   codeMap'        <- removeFromMap symbol <$> getCodeMap
-  liftIO $ saveMap (mapFileName romFile) codeMap'
+  liftIO (saveMap (mapFileName romFile) codeMap')
   setCodeMap codeMap'
 doCommand ListSymbols = do
   symbols <- listSymbols <$> getCodeMap
-  for_ symbols $ \(label, value) -> liftIO $ putStrLn $ label ++ ": " ++ formatHex value
+  for_ symbols $ \(label, value) -> liftIO (putStrLn (label ++ ": " ++ formatHex value))
