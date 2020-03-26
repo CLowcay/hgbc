67d1d91c3ec8feaaad81002c72ccd06a215845b1
Callum Lowcay <cwslowcay@gmail.com>
2020-03-27 11:07:45 +1300

    Optimisation

--- hgbc/src/Machine/GBC/Graphics.hs
+++ hgbc/src/Machine/GBC/Graphics.hs
@@ -64,12 +64,12 @@ data GraphicsState = GraphicsState {
   , frameBufferBytes :: !(Ptr Word8)
 
   -- | A temporary area for blending the background, window, and sprites.
-  , lineAssemblySpace :: !(VUM.IOVector PixelInfo)
+  , assemblySpace :: !(VUM.IOVector PixelInfo)
   -- | Each byte in this buffer corresponds to one of the pixels in the
   -- lineAssembly space. When a sprite pixel is drawn, the x-position of that
   -- sprite is written to the spritePriorityBuffer. This allows us to simulate
   -- the DMG behavior where sprites with lower x-positions take priority.
-  , spritePriorityBuffer :: !(VUM.IOVector Int8)
+  , priorityBuffer :: !(VUM.IOVector Int8)
 }
 
 type Index = Word8     -- Values ranging from 0 ~ 3
@@ -138,8 +138,8 @@ initGraphics vram modeRef frameBufferBytes portIF = mdo
   portVBK <- cgbOnlyPort modeRef 0x00 0x01
     $ \_ vbk' -> vbk' <$ setVRAMBank vram (if vbk' .&. 1 == 0 then 0 else 0x2000)
 
-  lineAssemblySpace    <- VUM.replicate 168 (0, (0, 0, False))
-  spritePriorityBuffer <- VUM.replicate 168 0
+  assemblySpace  <- VUM.replicate 168 (0, (0, 0, False))
+  priorityBuffer <- VUM.replicate 168 0
 
   pure GraphicsState { .. }
 
@@ -261,7 +261,8 @@ graphicsStep graphicsState@GraphicsState {..} graphicsSync clockAdvance = do
 
         when (mode' == HBlank) $ do
           let outputBase = frameBufferBytes `plusPtr` (fromIntegral line' * 640)
-          renderLine graphicsState line' lineAssemblySpace spritePriorityBuffer outputBase
+          emulatorMode <- readIORef modeRef
+          renderLine graphicsState emulatorMode line' outputBase
           setVRAMAccessible vram True
 
         when (mode' == VBlank) $ do
@@ -334,10 +335,8 @@ getSpriteBlendInfo attrs =
       cgbPalette = (attrs .&. 7) .<<. 2
   in  (layer, cgbPalette, isFlagSet flagDisplayPriority attrs)
 
-renderLine
-  :: GraphicsState -> Word8 -> VUM.IOVector PixelInfo -> VUM.IOVector Int8 -> Ptr Word8 -> IO ()
-renderLine GraphicsState {..} line assemblySpace priorityBuffer outputBase = do
-  mode <- readIORef modeRef
+renderLine :: GraphicsState -> EmulatorMode -> Word8 -> Ptr Word8 -> IO ()
+renderLine GraphicsState {..} mode line outputBase = do
   scx  <- readPort portSCX
   scy  <- readPort portSCY
   wx   <- readPort portWX
@@ -359,7 +358,7 @@ renderLine GraphicsState {..} line assemblySpace priorityBuffer outputBase = do
 
     let bgTiles = if isFlagSet flagBackgroundTileMap lcdc then 0x1C00 else 0x1800
     let bgTileLine = bgTiles + (32 * yTile)
-    tileMachine mode bgTileLine fontOffset yOffset windowStart xTile (negate xOffset)
+    tileMachine bgTileLine fontOffset yOffset windowStart xTile (negate xOffset)
 
   -- Render window data to the assembly area.
   when windowEnabled $ do
@@ -368,11 +367,11 @@ renderLine GraphicsState {..} line assemblySpace priorityBuffer outputBase = do
 
     let windowTiles    = if isFlagSet flagWindowTileMap lcdc then 0x1C00 else 0x1800
     let windowTileLine = windowTiles + (32 * yTileWindow)
-    tileMachine mode windowTileLine fontOffset yOffsetWindow 160 0 windowStart
+    tileMachine windowTileLine fontOffset yOffsetWindow 160 0 windowStart
 
   -- Render sprite data to the assembly area.
   let spriteHeight = if isFlagSet flagOBJSize lcdc then 16 else 8
-  when spritesEnabled $ doSprites mode spriteHeight
+  when spritesEnabled $ doSprites spriteHeight
 
   -- Copy the data from the assembly area into the frame texture buffer,
   -- applying the GB palettes.
@@ -385,7 +384,7 @@ renderLine GraphicsState {..} line assemblySpace priorityBuffer outputBase = do
   -- 
   -- Each round takes the offset to the tile to render, and the x position to
   -- render the pixels to.
-  tileMachine !mode !tileBase !fontOffset !yOffset !stopOffset = go
+  tileMachine !tileBase !fontOffset !yOffset !stopOffset = go
    where
     go !inPos !outPos = if outPos >= stopOffset
       then pure ()
@@ -425,15 +424,15 @@ renderLine GraphicsState {..} line assemblySpace priorityBuffer outputBase = do
         if byteOffset == 0 then pure (pixelOffset + 1) else go (byteOffset - 1) (pixelOffset + 1)
 
   -- Write sprites to the assembly area.
-  doSprites !mode !h = go 0 0
+  doSprites !h = go 0 0
    where
     go !offset !spritesRendered = if offset >= 40 || spritesRendered >= (10 :: Int)
       then pure ()
       else do
-        spriteRendered <- doSprite mode h (offset * 4)
+        spriteRendered <- doSprite h (offset * 4)
         go (offset + 1) (if spriteRendered then spritesRendered + 1 else spritesRendered)
 
-  doSprite !mode !h !offset = do
+  doSprite !h !offset = do
     (y, x) <- readSpritePosition vram offset
     let spriteVisible = x /= 0 && line + 16 >= y && line + 16 < y + h
     if not spriteVisible
